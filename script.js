b7bde34d2272db26b78f10b4696a49e8fdfbddb9
betrion <forteee@gmail.com>
2022-04-23 14:59:27 +0200

    Fixed typo and added keyboard event listen

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
 let numBox = document.getElementById("guessNum");
 let numOfTries = 1;
 let gameWon = false;
+let randomNum = randomizeNumber();
+
 let guessNumBtn = document
   .querySelector("#submitBtn")
   .addEventListener("click", () => {
     let resultBox = document.querySelector("#result");
     console.log(randomNum);
     if (numBox.value < randomNum) {
-      resultBox.innerHTML = "traženi broj je manji od unešenog";
+      resultBox.innerHTML = "Unešeni broj je manji od traženog";
       numOfTries += 1;
     } else if (numBox.value > randomNum) {
-      resultBox.innerHTML = "traženi broj je veći od unešenog";
+      resultBox.innerHTML = "Unešeni broj je veći od traženog";
       numOfTries += 1;
       console.log(numOfTries);
     } else {
       resultBox.innerHTML = `<strong>bravo, pogodili ste iz ${numOfTries}. puta</strong>`;
     }
   });
-let randomNum = randomizeNumber();
+numBox.addEventListener("keyup", (e) => {
+  if (e.keyCode === 13) {
+    document.querySelector("#submitBtn").click();
+  }
+});
 console.log(randomNum);
 function randomizeNumber() {
   return Math.floor(Math.random() * 100);
